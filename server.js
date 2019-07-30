8b3e8b4aeea2c9b85f0cd5c34ef5e136b9806966
Marine809 <rthorpesr@verizon.net>
2019-07-30 10:03:48 -0400

    updated link for FriendFinder

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.json());
+
+
 
 //ROUTERS
 require('./app/routing/apiRoutes.js')(app); 
